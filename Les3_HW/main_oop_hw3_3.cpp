23b388a5d041285d0134ac0dd7a07115165746d5
Козицын С.А <KozitsinSA@GROUP-SZN.local>
2021-05-26 18:06:33 +1000

    Les3_HW
    На проверку

---
+++ Les3_HW/main_oop_hw3_3.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+
+/*
+
+Создать класс: Fraction (дробь). Дробь имеет числитель и знаменатель (например, 3/7 или 9/2).
+
+Предусмотреть, чтобы знаменатель не был равен 0.
+
+Перегрузить: математические бинарные операторы (+, -, *, /) для выполнения действий с дробями.
+
+унарный оператор (-).
+
+логические операторы сравнения двух дробей (==, !=, <, >, <=, >=).
+
+Примечание: Поскольку операторы < и >=, > и <= — это логические противоположности, попробуйте перегрузить один через другой.
+
+Продемонстрировать использование перегруженных операторов.
+
+*/
+
+class Fraction
+{
+private:
+	double _numerator = 0;
+	double _denominator = 0;
+
+public:
+	Fraction() {}
+
+	Fraction(int numerator, int denominator)
+		:_numerator(numerator), _denominator(denominator)
+	{
+
+		if (_denominator == 0)
+
+		{
+			std::cout << "Error! The denominator cannot be zero" << std::endl;
+		}
+	}
+
+	Fraction operator+(const Fraction& other) const
+
+	{
+		Fraction tempAdd;
+		Fraction tempAdd1;
+		if (_denominator != other._denominator)
+		{
+			tempAdd._denominator = _denominator * other._denominator;
+			tempAdd._numerator = _numerator * other._denominator;
+			tempAdd1._numerator = _denominator * other._numerator;
+			tempAdd._numerator = tempAdd._numerator + tempAdd1._numerator;
+		}
+		else
+		{
+			tempAdd._numerator = _numerator + other._numerator;
+			tempAdd._denominator = _denominator;
+		}
+		return tempAdd;
+	}
+
+	Fraction operator-(const Fraction& other) const
+
+	{
+		Fraction tempSub;
+		Fraction tempSub1;
+		if (_denominator != other._denominator)
+		{
+			tempSub._denominator = _denominator * other._denominator;
+			tempSub._numerator = _numerator * other._denominator;
+			tempSub1._numerator = _denominator * other._numerator;
+			tempSub._numerator = tempSub._numerator - tempSub1._numerator;
+		}
+		else
+		{
+			tempSub._numerator = _numerator - other._numerator;
+			tempSub._denominator = _denominator;
+		}
+		return tempSub;
+	}
+
+	Fraction operator*(const Fraction& other) const
+
+	{
+		Fraction tempMult;
+
+		tempMult._denominator = _denominator * other._denominator;
+		tempMult._numerator = _numerator * other._numerator;
+
+		return tempMult;
+	}
+
+	Fraction operator/(const Fraction& other) const
+
+	{
+		Fraction tempDiv;
+
+		tempDiv._numerator = _numerator * other._denominator;
+		tempDiv._denominator = _denominator * other._numerator;
+
+		return tempDiv;
+	}
+
+		Fraction operator-() const
+	{
+		return Fraction(-_numerator, -_denominator);
+	}
+
+	Fraction operator==(const Fraction& other) const
+
+	{
+		Fraction tempEqu;
+		if ((tempEqu._numerator = _numerator == other._numerator) &&
+			(tempEqu._denominator = _denominator == other._denominator))
+		{
+			std::cout << "Fractions are equal" << std::endl;
+		}
+		else
+		{
+			std::cout << "Fractions are not equal" << std::endl;
+		}
+		return tempEqu;
+	}
+
+	Fraction operator<(const Fraction& other) const
+
+	{
+		Fraction tempEqu;
+		if ((_numerator / _denominator) < (other._numerator / other._denominator))
+		{
+			std::cout << "frac1 is less than frac2" << std::endl;
+		}
+		else
+		{
+				std::cout << "frac1 is greater than frac2" << std::endl;
+		}
+		return tempEqu;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction &frac)
+	{
+		out << frac._numerator << "/" << frac._denominator;
+		return out;
+	}
+};
+
+int main()
+
+{
+	Fraction frac1(2, 3);
+	Fraction frac2(5, 8);
+	
+	std::cout << frac1 << " + " << frac2 << " = " << frac1 + frac2 << std::endl;
+	std::cout << frac1 << " - " << frac2 << " = " << frac1 - frac2 << std::endl;
+	std::cout << frac1 << " * " << frac2 << " = " << frac1 * frac2 << std::endl;
+	std::cout << frac1 << " : " << frac2 << " = " << frac1 / frac2 << std::endl;
+	std::cout << "negative value of fraction " << frac1 << " is " << - frac1 << std::endl;
+	Fraction resultEqu = frac1 == frac2;
+	Fraction resultLess = frac1 < frac2;
+
+	return 0;
+
+}
---
+++ Les3_HW/main_oop_hw3_4.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+/*
+Создать класс Card, описывающий карту в игре БлэкДжек. У этого класса должно быть три поля: масть, значение карты 
+и положение карты (вверх лицом или рубашкой). Сделать поля масть и значение карты типом перечисления (enum). 
+Положение карты - тип bool. Также в этом классе должно быть два метода:
+метод Flip(), который переворачивает карту, т.е. если она была рубашкой вверх, то он ее поворачивает лицом вверх, и наоборот.
+метод GetValue(), который возвращает значение карты, пока можно считать, что туз = 1.
+
+Рубашка (shirt) — узор на обратной стороне карты.
+Масть (suit) —  пики, трефы, черви и буби (spade, club, heart и diamond)
+Значение карты (cardValue) — валет, дамы, короля, туз, джокер и от 1 до 9 (Jack, Queen, King, Ace, Joker).
+*/
+
+class Card
+{
+private:
+	bool shirt = true;
+	int suit  = 0;
+	int cardValue = 0;
+public:
+	enum Suit
+	{
+		SPADE,
+		CLUB,
+		HEART,
+		DIAMOND,
+	};
+
+	enum CardValue
+	{
+		ONE,
+		ACE,
+		TWO,
+		THREE,
+		FOUR,
+		FIVE,
+		SIX,
+		SEVEN,
+		EIGHT,
+		NINE,
+		JACK,
+		QUEEN,
+		KING,
+		JOKER
+	};
+
+	void Flip()
+	{
+		if (shirt == true)
+		{
+			shirt = false;
+		}
+	}
+
+	int getValue() { return ACE; }
+
+	void printValue()
+	{
+		std::cout << ACE << std::endl;
+	}
+};
+
+int main()
+{
+	Card card;
+	card.getValue();
+	card.printValue();
+	
+
+
+	return 0;
+}
